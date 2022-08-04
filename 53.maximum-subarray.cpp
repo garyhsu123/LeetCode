efc4e536958f4aee0444e40d2c8316e6a7123115
Gary_Hsu <chun52117@gmail.com>
2022-08-04 14:14:56 -0700

    [53] Maximum Subarray
    [Link] https://leetcode.com/problems/maximum-subarray/description/

---
+++ 53.maximum-subarray.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=53 lang=cpp
+ *
+ * [53] Maximum Subarray
+ */
+
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+// @lc code=start
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+        int *dp = new int[nums.size()];
+        int output = dp[0] = nums[0];
+        for (int i = 1; i < nums.size(); ++i) {
+            dp[i] = max(dp[i-1] + nums[i], nums[i]);
+            output = max(output, dp[i]);
+        }
+        return output;
+    }
+};
+// @lc code=end
+
