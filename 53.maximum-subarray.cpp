a484512514609399a54bc72e0526385225c12327
Gary_Hsu <chun52117@gmail.com>
2022-08-07 21:21:12 -0700

    [53] Maximum Subarray
    [Link] https://leetcode.com/problems/maximum-subarray/description/
    [Tags] #Dynamic Programming
    [Note] The dp[i] is used to store the max sum value of subarry where the index is from 0 to i and it includes the nums[i].

--- 53.maximum-subarray.cpp
+++ 53.maximum-subarray.cpp
@@ -11,6 +11,22 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
+    /*
+    int maxSubArray(vector<int>& nums) {
+        int maxSubArray = INT_MIN;
+        int acc = INT_MIN;
+        for (int num: nums) {
+            if (acc > 0) {
+                acc += num;
+            } 
+            else {
+                acc = num;
+            }
+            if (acc > maxSubArray) maxSubArray = acc;
+        }
+        return maxSubArray;
+    }
+    */
     int maxSubArray(vector<int>& nums) {
         int *dp = new int[nums.size()];
         int output = dp[0] = nums[0];
