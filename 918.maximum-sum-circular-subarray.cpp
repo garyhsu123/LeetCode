fe3e1f1275edee436dc139d181fb87ce526c5e4e
Gary_Hsu <chun52117@gmail.com>
2022-08-07 22:09:44 -0700

    [918] Maximum Sum Circular Subarray
    [Link] https://leetcode.com/problems/maximum-sum-circular-subarray/discuss/1587500
    [Tags] #Dynamic Programming #Kadane's Algorithm
    [Note] Implement Kadane's Algorithm.

--- 918.maximum-sum-circular-subarray.cpp
+++ 918.maximum-sum-circular-subarray.cpp
@@ -8,6 +8,7 @@ using namespace std;
 // @lc code=start
 class Solution {
 public:
+    /*
     int maxSubarraySumCircular(vector<int>& nums) {
         int sum = 0;
         int maxSub = INT_MIN;
@@ -37,6 +38,30 @@ public:
 
         return max(maxSub, sum - minSub ?: maxSub);
 
+    }
+    */
+    int maxSubarraySumNonCircular(vector<int>& nums) {
+        int maxSub = INT_MIN;
+        int acc = INT_MIN;
+        for (int num: nums) {
+            if (acc < 0) acc = 0;
+            acc += num;
+            maxSub = max(maxSub, acc);
+        }
+        return maxSub;
+    }
+    
+    int maxSubarraySumCircular(vector<int>& nums) {
+        int nonCircularSum = maxSubarraySumNonCircular(nums);
+        int sum = 0;
+        for (int &num: nums) {
+            sum += num;
+            num *= -1;
+        }
+        int circularSum = sum + maxSubarraySumNonCircular(nums);
+
+        if (circularSum == 0) return nonCircularSum;
+        return max(nonCircularSum, circularSum);
     }
 };
 // @lc code=end
