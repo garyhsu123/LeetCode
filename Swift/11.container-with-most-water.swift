a99998893a5ce246a1561c50503a798e22660618
Gary_Hsu <chun52117@gmail.com>
2024-06-07 14:28:19 -0700

    [11] Container With Most Water
    [Link] https://leetcode.com/problems/container-with-most-water/solutions/
    [Tags]
    [Note] Redo

--- Swift/11.container-with-most-water.swift
+++ Swift/11.container-with-most-water.swift
@@ -7,25 +7,18 @@
 // @lc code=start
 class Solution {
     func maxArea(_ height: [Int]) -> Int {
-        
-        var leftIdx = 0
-        var rightIdx = height.count - 1
+        var maxAmount = 0
+        var left = 0
+        var right = height.count - 1
 
-        var curMaxArea = min(height[leftIdx], height[rightIdx]) * (rightIdx - leftIdx)
-
-        
-        while (rightIdx > leftIdx) {
-            if height[rightIdx] > height[leftIdx] {
-                leftIdx += 1
-            }
-            else {
-                rightIdx -= 1
-            }
-            curMaxArea = max(curMaxArea, min(height[leftIdx], height[rightIdx]) * (rightIdx - leftIdx))
+        while left <= right {
+            let amount = min(height[left], height[right]) * (right - left)
+            maxAmount = max(maxAmount, amount)
+            if height[left] >= height[right] { right -= 1 }
+            else { left += 1}
         }
 
-
-        return curMaxArea
+        return maxAmount
     }
 }
 // @lc code=end
